3ab8f93954eb5721517ea51731056a1ea0bed103
hasan-bro-coder <hasanur23910@gmail.com>
2024-04-22 13:46:56 +0600

    74

--- src/components/credits.vue
+++ src/components/credits.vue
@@ -1,5 +1,13 @@
 <template>
-
+<div class="grid">
+    <div class="col">
+        <h1>smart outfit</h1>
+        <p>made by hasan</p>
+    </div>
+    <div class="col">
+        <h1>contact us</h1>
+    </div>
+</div>
     
 </template>
 <script>
