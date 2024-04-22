4b1c7c2bcdbc37d8cf1aca1b32ed29b4cdac5a56
hasan-bro-coder <hasanur23910@gmail.com>
2024-04-22 13:00:51 +0600

    29

--- src/views/order.vue
+++ src/views/order.vue
@@ -281,7 +281,6 @@ export default {
   align-items: center;
   padding: 20%;
   form {
-    text-transform: uppercase !important;
     width: 100%;
     height: 100%;
     display: flex;
@@ -295,12 +294,15 @@ export default {
       font-family:monospace;
       font-size: 16px
     }
+    label{
+      text-transform: uppercase !important;
+    }
     .con {
       display: flex;
       flex-direction: column;
       gap: 10px;
       width: 100%;
-      height: 100%;
+      height: 60px;
     }
 
     .inpgrp {
