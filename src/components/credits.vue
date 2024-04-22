962475ae40e09f77cb450b7a13374cf76c7d2be8
hasan-bro-coder <hasanur23910@gmail.com>
2024-04-22 13:50:04 +0600

    77

--- src/components/credits.vue
+++ src/components/credits.vue
@@ -1,29 +1,30 @@
 <template>
-<div class="grid">
-    <div class="col">
-        <h1>smart outfit</h1>
-        <p>made by hasan</p>
+    <div class="grid">
+        <div class="col">
+            <h1>smart outfit</h1>
+            <p>made by hasan</p>
+        </div>
+        <div class="col">
+            <h1>contact us</h1>
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+            </ul>
+        </div>
+        <div class="col">
+            <h1>quick links</h1>
+            <a href="">/admin</a>
+        </div>
+        <div class="col">
+            <h1>Social page</h1>
+            <a href="">facebook</a>
+            <a href="">insta</a>
+            <a href="">twitter</a>
+            <a href="">youtube</a>
+        </div>
     </div>
-    <div class="col">
-        <h1>contact us</h1>
-        <ul>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-        </ul>
-    </div>
-    <div class="col">
-        <h1>quick links</h1>
-        <a href="">/admin</a>
-    </div>
-    <div class="col">
-        <h1>Social page</h1>
-        <a href="">facebook</a>
-        <a href="">insta</a>
-        <a href="">twitter</a>
-    </div>
-</div>
-    
+
 </template>
 <script>
 export default {
