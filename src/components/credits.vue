03692ceb3d08ca381ee26b0aef0342fb322721b5
hasan-bro-coder <hasanur23910@gmail.com>
2024-04-22 13:47:59 +0600

    75

--- src/components/credits.vue
+++ src/components/credits.vue
@@ -6,6 +6,18 @@
     </div>
     <div class="col">
         <h1>contact us</h1>
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
+    </div>
+    <div class="col">
+        <h1>quick links</h1>
+        <a href="">/admin</a>
+    </div>
+    <div class="col">
+        <h1></h1>
     </div>
 </div>
     
