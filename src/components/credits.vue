48c685975631f195478a69583535f7bfff5bd92d
hasan-bro-coder <hasanur23910@gmail.com>
2024-04-25 18:14:35 +0600

    1

--- src/components/credits.vue
+++ src/components/credits.vue
@@ -14,7 +14,7 @@
         </div>
         <div class="col">
             <h1>quick links</h1>
-            <router-link to="/admin" style="color: #00bdd6 !important;">admin page</router-link>
+            <a href="/admin" style="color: #00bdd6 !important;">admin page</a>
         </div>
         <div class="col">
             <h1>Social page</h1>
