355c6ffd885df8669a2a6d584835bd64dbf3c67c
hasan-bro-coder <hasanur23910@gmail.com>
2024-05-01 23:27:13 +0600

    29

--- src/components/admin_product.vue
+++ src/components/admin_product.vue
@@ -66,7 +66,7 @@
     <div class="product" v-for="product in products" :key="product._id" @click.self="send(product._id)"
       data-aos="fade-up" data-aos-delay="100">
       <img @click.self="send(product._id)"
-        :src="product.image || 'https://images.unsplash.com/photo-1711809657132-fa38bf2ac5e7?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'"
+        :src="product.image"
         alt="image not found" />
       <div class="bottom">
         <div class="det">
